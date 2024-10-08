1c4cb0e9bc26ec13a9a5c37f8983b886effa2688
kuroma7 <mkuroda@aicom.co.jp>
2024-10-08 10:01:20 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,9 +1,6 @@
 const btn = document.getElementById(`btn`);
 const text = document.getElementById(`text`);
 
-console.log(btn);
-console.log(text);
-
 btn.addEventListener(`click`, ()=>{
     text.textContent=`ボタンをクリックしました`;
 });
